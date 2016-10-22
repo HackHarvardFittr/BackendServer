0bf2386dbed77685837b8e6a3bf76ba91aa6a3c5
aayushshah15 <aayush.shah15@gmail.com>
2016-10-22 00:03:52 -0400

    image upload test

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
 var express = require('express')
 var app = express()
 var mongoose  = require('mongoose')
+var multer = require('multer')
+var upload = multer({ dest : 'uploads/'})
 var cloudinary = require('cloudinary')
 
 app.get('/', function(req, res) {
 	console.log('request received')
 	res.send('This works!')
+})
+
+app.post('/upload', upload.array('file', 12), function(req, res) {
+	console.log(req.files)
+	res.send("done")
 })
 
 app.listen(4900)
